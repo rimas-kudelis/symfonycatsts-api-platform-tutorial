47e0a0b93e9eeb8de892f9a12748a166770fc0fe
Rimas Kudelis <rimas.kudelis@adeoweb.biz>
2021-12-15 16:26:41 +0200

    P2C11: logging in inside tests

---
+++ tests/Functional/CheeseListingResourceTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
+
+class CheeseListingResourceTest extends ApiTestCase
+{
+    public function testCreateCheeseListing(): void
+    {
+        $client = static::createClient();
+        $response = $client->request('POST', '/api/cheeses');
+
+        $this->assertResponseStatusCodeSame(401);
+
+        $user = new User();
+        $user->setEmail('cheeseplease@example.com');
+        $user->setUsername('cheeseplease');
+        $user->setPassword('$argon2id$v=19$m=65536,t=4,p=1$Rg2+xerLwieLBVX3l0p+2A$d+biDK6NINKB60tkoPOLxJgTVXgKMc8DcC4vXvQGtbo');
+
+        $em = static::getContainer()->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $client->request('POST', '/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => 'cheeseplease@example.com',
+                'password' => 'foo',
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(204);
+    }
+}
