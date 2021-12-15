2dcbc73dbb102a0fca149bd614a4c3cdb12471bb
Rimas Kudelis <rimas.kudelis@adeoweb.biz>
2021-12-15 18:52:03 +0200

    P2C15: base class for API tests

---
+++ src/Test/ApiTestCase.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase as BaseApiTestCase;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
+use App\Entity\User;
+
+class ApiTestCase extends BaseApiTestCase
+{
+    protected function createUser(string $email, string $password): User
+    {
+        $user = new User();
+        $user->setEmail($email);
+        $user->setUsername(substr($email, 0, strpos($email, '@')));
+        $encodedPassword = static::getContainer()->get('security.password_encoder')
+            ->encodePassword($user, $password);
+        $user->setPassword($encodedPassword);
+
+        $em = static::getContainer()->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $user;
+    }
+
+    protected function logIn(Client $client, string $email, string $password): void
+    {
+        $client->request('POST', '/login', [
+            'json' => [
+                'email' => $email,
+                'password' => $password,
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(204);
+    }
+
+    protected function createUserAndLogIn(Client $client, string $email, string $password): User
+    {
+        $user = $this->createUser($email, $password);
+
+        $this->logIn($client, $email, $password);
+
+        return $user;
+    }
+}
--- tests/Functional/CheeseListingResourceTest.php
+++ tests/Functional/CheeseListingResourceTest.php
@@ -2,8 +2,7 @@
 
 namespace App\Tests\Functional;
 
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
-use App\Entity\User;
+use App\Test\ApiTestCase;
 use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
 class CheeseListingResourceTest extends ApiTestCase
@@ -13,26 +12,14 @@ class CheeseListingResourceTest extends ApiTestCase
     public function testCreateCheeseListing(): void
     {
         $client = static::createClient();
-        $response = $client->request('POST', '/api/cheeses');
-
+        $client->request('POST', '/api/cheeses');
         $this->assertResponseStatusCodeSame(401);
 
-        $user = new User();
-        $user->setEmail('cheeseplease@example.com');
-        $user->setUsername('cheeseplease');
-        $user->setPassword('$argon2id$v=19$m=65536,t=4,p=1$Rg2+xerLwieLBVX3l0p+2A$d+biDK6NINKB60tkoPOLxJgTVXgKMc8DcC4vXvQGtbo');
+        $this->createUserAndLogIn($client, 'cheeseplease@example.com', 'foo');
 
-        $em = static::getContainer()->get('doctrine')->getManager();
-        $em->persist($user);
-        $em->flush();
-
-        $client->request('POST', '/login', [
-            'headers' => ['Content-Type' => 'application/json'],
-            'json' => [
-                'email' => 'cheeseplease@example.com',
-                'password' => 'foo',
-            ],
+        $client->request('POST', '/api/cheeses', [
+            'json' => [],
         ]);
-        $this->assertResponseStatusCodeSame(204);
+        $this->assertResponseStatusCodeSame(422);
     }
 }
