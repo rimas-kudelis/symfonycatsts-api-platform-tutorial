04cb80f0ed40d79f0d5697b434d3e1d5f813f4bb
Rimas Kudelis <rimas.kudelis@adeoweb.biz>
2021-12-15 14:09:38 +0200

    P2C04: Authentication errors

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,8 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: 'POST')]
-    public function login()
+    public function login(): Response
     {
+        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->json([
+                'error' => 'Invalid login request, use the application/json content type.',
+            ], 400);
+        }
         return $this->json([
             'user' => $this->getUser()?->getId(),
         ]);
