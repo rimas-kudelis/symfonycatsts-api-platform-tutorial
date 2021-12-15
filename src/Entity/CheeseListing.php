f7206c82ee33afbe6fe091f69714591f0e5b9ab7
Rimas Kudelis <rimas.kudelis@adeoweb.biz>
2021-12-15 19:33:22 +0200

    P2C18: Access control and voters

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -27,11 +27,11 @@ use Symfony\Component\Validator\Constraints as Assert;
             "normalization_context" => ["groups" => ["cheese_listing:read", "cheese_listing:item:get"]]
         ],
         "put" => [
-            "security" => "is_granted('ROLE_USER') and object.getOwner() == user",
+            "security" => "is_granted('EDIT', object)",
             "security_message" => "Only the creator can edit a cheese listing.",
         ],
         "patch" => [
-            "security" => "is_granted('ROLE_USER') and object.getOwner() == user",
+            "security" => "is_granted('EDIT', object)",
             "security_message" => "Only the creator can edit a cheese listing.",
         ],
         "delete" => [
---
+++ src/Security/Voter/CheeseListingVoter.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\CheeseListing;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class CheeseListingVoter extends Voter
+{
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    protected function supports(string $attribute, $subject): bool
+    {
+        return in_array($attribute, ['EDIT'])
+            && $subject instanceof CheeseListing;
+    }
+
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        /** @var CheeseListing $subject */
+
+        switch ($attribute) {
+            case 'EDIT':
+                if ($user === $subject->getOwner()) {
+                    return true;
+                }
+
+                if ($this->security->isGranted('ROLE_ADMIN')) {
+                    return true;
+                }
+
+                return false;
+        }
+
+        throw new \LogicException(sprintf('Unhandled attribute "%s".', $attribute));
+    }
+}
