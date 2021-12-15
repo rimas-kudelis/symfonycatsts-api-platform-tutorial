2f8a439a598f952c50f0456dc933bea3921fea94
Rimas Kudelis <rimas.kudelis@adeoweb.biz>
2021-12-15 14:03:25 +0200

    P2C3: login with json_login

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/login', name: 'app_login', methods: 'POST')]
+    public function login()
+    {
+        return $this->json([
+            'user' => $this->getUser()?->getId(),
+        ]);
+    }
+}
