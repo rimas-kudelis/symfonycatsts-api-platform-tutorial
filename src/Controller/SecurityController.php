154292132316088d4aac6f581382469950bde3fe
Rimas Kudelis <rimas.kudelis@adeoweb.biz>
2021-12-15 14:37:19 +0200

    P2C06: On authentication success

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use ApiPlatform\Core\Api\IriConverterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: 'POST')]
-    public function login(): Response
+    public function login(IriConverterInterface $iriConverter): Response
     {
         if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->json([
                 'error' => 'Invalid login request, use the application/json content type.',
             ], 400);
         }
-        return $this->json([
-            'user' => $this->getUser()?->getId(),
+
+        return new Response(null, 204, [
+            'Location' => $iriConverter->getIriFromItem($this->getUser()),
         ]);
     }
 }
